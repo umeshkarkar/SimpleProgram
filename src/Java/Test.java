6acb11865926a9c49c26d2408926e311de1c1351
umesh <umeshkarkar22@gmail.com>
2020-06-16 14:55:50 +0530

    Umesh commited

--- src/Java/Test.java
+++ src/Java/Test.java
@@ -11,9 +11,14 @@ package Java;
 		 e1.insert(1, "Umesh", 45);
 		 e2.insert(2, "karkar", 66);
 		 e3.insert(3, "PKTTT", 77);
+		 e3.insert(3, "PKTTT", 77);
+		 e3.insert(3, "PKTTT", 77);
 		 
 		 e1.display();
 		 e2.display();
 		 e3.display();
+		 e3.display();
+		 e3.display();
+		 
 	 }
 }
